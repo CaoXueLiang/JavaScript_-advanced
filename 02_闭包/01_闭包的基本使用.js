b5624d73aa7ade06740512e27278617baa950d03
_誌念 <2357747972@qq.com>
2021-10-14 09:31:59 +0000

    ES6语法

---
+++ 02_闭包/01_闭包的基本使用.js
@@ -0,0 +1,34 @@
+// var scope = "global scope";
+// function checkscope() {
+//   var scope = "local scope";
+//   function f() {
+//     console.log(scope);
+//   }
+//   return f;
+// }
+
+// var foo = checkscope();
+// foo();
+
+// console.log(foo);
+
+// function foo() {
+//   console.log("foo");
+// }
+
+// var foo = 1;
+
+console.log(foo);
+
+foo();
+
+function foo() {
+  console.log("foo");
+}
+
+foo();
+var foo = 1;
+
+console.log(foo);
+console.log(typeof foo);
+foo();
